e464642801b4ce0160cca5a84560ffa49b23af92
maciu <maciu@DESKTOP-PJ8TGSP>
2022-01-21 12:35:52 +0200

    All

---
+++ src/Basics/Tryouts.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class Tryouts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Tryouts myobj = new Tryouts();
+		
+		System.out.println(myobj.sum(3, 5));
+		
+	}
+
+	public int sum(int a, int b) {
+		
+		
+		return a+ b;
+		
+	}
+}
+
+
